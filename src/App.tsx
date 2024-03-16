e762b5dcddf7bb30958f2584c3cef4b1cc8d5bdd
sonDang612 <sonhoangdang0612@gmail.com>
2024-03-16 20:32:46 +0700

    feat: setup short path import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Row } from "~/components";
+
 function App() {
-  return <div className="text-red-500">Hello World</div>;
+  return (
+    <div className="text-red-500">
+      <Row />
+    </div>
+  );
 }
 
 export default App;
